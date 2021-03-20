391fa775466df5c2a161bbc20329d9c1a00a8fd8
Guillaume-dm <56759820+Guillaume-dm@users.noreply.github.com>
2021-03-20 13:17:26 +0100

    Create hero.php
    Ajout du fichier hero.php permettant l'affichage de bienvenue lors de chacun des onglets de mon porte-folio.

---
+++ includes/hero.php
@@ -0,0 +1,11 @@
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex flex-column justify-content-center align-items-left">
+    <div class="container">
+        <div class="hero-container" data-aos="fade-in">
+            <h1>Guillaume Dumillier</h1>
+            <h3>Je suis étudiant en BTS SIO option SLAM et je vous accueille sur mon portefolio 😙</h3>
+            <p>Je reste à toute disposition pour éclairer vos questions. 😊.</p>
+        </div>
+    </div>
+</section>
+<!-- End Hero -->
