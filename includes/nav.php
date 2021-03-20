b81e7c8dffe22f79b1f4a5e45288ba93b0d4a335
Guillaume-dm <56759820+Guillaume-dm@users.noreply.github.com>
2021-03-20 13:19:11 +0100

    Create nav.php
    Ajout de l'ensemble du fichier nav.php permettant la génération de l'ensemble des barres de navigation de mon porte-folio.

---
+++ includes/nav.php
@@ -0,0 +1,36 @@
+<!-- ======= Header ======= -->
+    <header id="header">
+        <div class="d-flex flex-column">
+
+            <div class="profile">
+                <img src="assets/img/profile-img.jpg" alt="" class="img-fluid rounded-circle">
+                <h1 class="text-light"><a href="index.html">Guillaume DUMILLIER</a></h1>
+                <div class="social-links mt-3 text-center">
+                    <a href="https://www.linkedin.com/in/guillaume-dumillier-0a6867208" class="linkedin" target="_blank"><i class="icofont-linkedin"></i></a>
+                    <a href="https://github.com/Guillaume-dm" class="github" target="_blank"><i class="icofont-github"></i></a>
+                    <a href="https://t.me/Returnofgd" class="telegram" target="_blank"><i class="icofont-telegram"></i></a>
+                    <a href="https://www.instagram.com/guillaume_425_enl" class="instagram" target="_blank"><i class="icofont-instagram"></i></a>
+                    </br>
+                    <a href="https://www.snapchat.com/add/guillaume_mov42" class="snapchat" target="_blank"><i class="icofont-snapchat"></i></a>
+                    <a href="https://www.facebook.com/guillaume.dumillier.3/?viewas=" class="facebook" target="_blank"><i class="icofont-facebook"></i></a>
+                    <a href="https://www.facebook.com/messages/t/100049730851689" class="messenger" target="_blank"><i class="icofont-facebook-messenger"></i></a>
+                </div>
+            </div>
+            <nav class="nav-menu">
+                <ul>
+                    <li><a href="https://gdumillier.ml/#hero"><i class="icofont-home"></i> <span>Accueil</span></a></li>
+                    <li><a href="https://gdumillier.ml/#about"><i class="icofont-user"></i> <span>A propos de moi</span></a></li>
+                    <li><a href="https://gdumillier.ml/#resume"><i class="icofont-computer"></i> <span>Veille technologique</span></a></li>
+                    <li><a href="https://gdumillier.ml/#portfolio"><i class="icofont-institution"></i>Veille juridique</a></li>
+                    <li><a href="https://gdumillier.ml/#contact"><i class="icofont-server"></i> Expériences & stages</a></li>
+                </ul>
+            </nav>
+            <!-- .nav-menu -->
+            <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+
+        </div>
+    </header>
+    <!-- End  Header -->
+
+    <main id="main">
