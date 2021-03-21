b79b21370670cf6bdabea493fce393ddb8d40f30
Guillaume-dm <56759820+Guillaume-dm@users.noreply.github.com>
2021-03-21 10:13:45 +0100

    Create politique-de-confidentialite.php
    Ajout du fichier qui permet l'affichage du contenu sur ma politique de confidentialité.

---
+++ politique-de-confidentialite.php
@@ -0,0 +1,196 @@
+<?php include("includes/head.php") ?>
+
+<?php include("includes/nav.php") ?>
+
+<?php include("includes/hero.php") ?>
+
+
+<!-- ======= Politique Section ======= -->
+<section id="politique" class="about py-5">
+    <div class="container" data-aos="fade-left">
+        <div class="section-title">
+            <h2>Mentions légales</h2>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>1. Présentation du site</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi : </br>
+
+            <strong>Propriétaire :</strong> DUMILLIER Guillaume </br>
+            <strong>Créateur :</strong> <a href="https://www.gdumillier.ml" target="_blank" rel="noopener">DUMILLIER Guillaume</a> </br>
+            <strong>Responsable publication :</strong> DUMILLIER Guillaume </br>
+            <strong>Webmaster :</strong> DUMILLIER Guillaume </br>
+            <strong>Hébergeur :</strong> infinityfree - Newcastle Upon Tyne, United Kingdom </br>
+
+
+            <strong>Crédits :</strong> Ce <a href="https://www.aetherium.fr/generateur-de-mentions-legales-et-de-politique-de-confidentialite/" target="_blank" rel="noopener">modèle d'informations légales</a> est offert par <a href="https://www.aetherium.fr/" target="_blank" rel="noopener">L'Aetherium, agence de communication en Côtes d'Armor</a>. La partie mentions légales est une adaptation de l’œuvre proposée par <a title="mentions legales" href="https://www.subdelirium.com/generateur-de-mentions-legales/" target="_blank" rel="noopener">SubDelirium</a>. La partie politique de confidentialité est basée sur une œuvre originale de <a title="mentions legales" href="https://www.aetherium.fr/generateur-de-mentions-legales-et-de-politique-de-confidentialite/" target="_blank" rel="noopener">L'Aetherium</a>.
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>2. Conditions générales d’utilisation du site et des services proposés</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            L’utilisation du site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> implique l’acceptation pleine et entière des conditions générales d’utilisation ci-après décrites. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment, les utilisateurs du site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> sont donc invités à les consulter de manière régulière.
+
+            Ce site est normalement accessible à tout moment aux utilisateurs. Une interruption pour raison de maintenance technique peut être toutefois décidée par DUMILLIER Guillaume, qui s’efforcera alors de communiquer préalablement aux utilisateurs les dates et heures de l’intervention. Le site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> est mis à jour régulièrement par DUMILLIER Guillaume.
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>3. Description des services fournis</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            Le site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> a pour objet de fournir une information concernant l’ensemble des activités de la société. DUMILLIER Guillaume s’efforce de fournir sur le site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> des informations aussi précises que possible. Toutefois, il ne pourra être tenue responsable des omissions, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations.
+
+            Tous les informations indiquées sur le site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> sont données à titre indicatif et sont susceptibles d’évoluer. Par ailleurs, les renseignements figurant sur le site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne.
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>4. Limitations contractuelles sur les données techniques</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            Le site utilise la technologie JavaScript. Le site Internet ne pourra être tenu responsable de dommages matériels liés à l’utilisation du site. De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis à jour
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>5. Propriété intellectuelle et contrefaçons</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            Sauf mention contraire, DUMILLIER Guillaume est propriétaire des droits de propriété intellectuelle ou détient les droits d’usage sur tous les éléments accessibles sur le site, notamment les textes, images, graphismes, logo, icônes.
+
+            Les textes et images présents sur le site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> produits par DUMILLIER Guillaume sont mis à disposition selon les termes de la <a href="https://creativecommons.org/licenses/by-nd/3.0/deed.fr" rel="nofollow">licence Creative Commons Paternité – Pas de Modification 3.0 non transcrit</a>. L’exploitation sur un autre site Internet d’un ou des textes présent sur <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> doit toutefois faire l’objet d’une demande d’autorisation auprès de DUMILLIER Guillaume, afin d'éviter les situations de duplicate content. Les autorisations au-delà du champ de cette licence peuvent être obtenues en contactant le propriétaire du site.
+
+            Toute exploitation non autorisée du site par les présentes mentions légales, ou d’un des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>6. Limitations de responsabilité</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            DUMILLIER Guillaume ne pourra être tenue responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a>, et résultant soit de l’utilisation d’un matériel ne répondant pas aux spécifications indiquées au point 4, soit de l’apparition d’un bug ou d’une incompatibilité.
+
+            DUMILLIER Guillaume ne pourra également être tenue responsable des dommages indirects (tels par exemple qu’une perte de marché ou perte d’une chance) consécutifs à l’utilisation du site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a>.
+
+            Des espaces interactifs (possibilité de poser des questions via mes liens sociaux) sont à la disposition des utilisateurs. DUMILLIER Guillaume se réserve le droit de supprimer, sans mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données. Le cas échéant, DUMILLIER Guillaume se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie, etc.).
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>7. Liens hypertextes et cookies</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            Le site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> contient un certain nombre de liens hypertextes vers d’autres sites, mis en place avec l’autorisation de DUMILLIER Guillaume. Cependant, DUMILLIER Guillaume n’a pas la possibilité de vérifier le contenu des sites ainsi visités, et n’assumera en conséquence aucune responsabilité de ce fait.
+
+            La navigation sur le site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> est susceptible de provoquer l’installation de cookies sur l’ordinateur de l’utilisateur. Un cookie est un fichier de petite taille, qui ne permet pas l’identification de l’utilisateur, mais qui enregistre des informations relatives à la navigation d’un ordinateur sur un site. Les données ainsi obtenues visent à faciliter la navigation ultérieure sur le site, et ont également vocation à permettre diverses mesures de fréquentation.
+
+            Le refus d’installation d’un cookie peut entraîner l’impossibilité d’accéder à certains services. L’utilisateur peut toutefois configurer son ordinateur de la manière suivante, pour refuser l’installation des cookies :
+            <ul>
+                <li></li>
+                <li>Sous Internet Explorer : onglet outil (pictogramme en forme de rouage en haut a droite) / options internet. Cliquez sur Confidentialité et choisissez Bloquer tous les cookies. Validez en cliquant sur Ok.</li>
+                <li>Sous Edge : cliquez en haut à droite du navigateur sur le pictogramme de menu (symbolisé par trois petits points). Sélectionnez Paramètres. Puis allez sur l'onglet Confidentialité et Sécurité. Dans la section Cookies, choisissez de bloquer les cookies.</li>
+                <li>Sous Firefox : en haut de la fenêtre du navigateur, cliquez sur le bouton Firefox, puis aller dans l'onglet Options. Cliquer sur l'onglet Vie privée. Paramétrez les Règles de conservation sur : utiliser les paramètres personnalisés pour l'historique. Enfin cochez la case pour bloquer les cookies.</li>
+                <li>Sous Safari : cliquez en haut à droite du navigateur sur le pictogramme de menu (symbolisé par un rouage). Sélectionnez Paramètres. Cliquez sur Afficher les paramètres avancés. Dans la section Confidentialité, cliquez sur Paramètres de contenu. Dans la section "Cookies", vous pouvez bloquer les cookies.</li>
+                <li>Sous Chrome : cliquez en haut à droite du navigateur sur le pictogramme de menu (symbolisé par trois lignes horizontales). Sélectionnez Paramètres. Cliquez sur Afficher les paramètres avancés. Dans la section Confidentialité, cliquez sur préférences. Dans l'onglet Confidentialité, vous pouvez bloquer les cookies.</li>
+            </ul>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>8. Droit applicable et attribution de juridiction</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            Tout litige en relation avec l’utilisation du site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> est soumis au droit français. Il est fait attribution exclusive de juridiction aux tribunaux compétents de Paris.
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>9. Les principales lois concernées</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            <ul>
+                <li>Loi n° 78-17 du 6 janvier 1978, notamment modifiée par la loi n° 2004-801 du 6 août 2004 relative à l'informatique, aux fichiers et aux libertés.</li>
+                <li>Loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique.</li>
+                <li>Règlement européen n° 2016/679 (dit <a href="https://www.aetherium.fr/rgpd-loi-de-finances-double-peine-impact-wordpress-ecommerce/" target="_blank" rel="noopener">règlement général sur la protection des données ou RGPD)</a>.</li>
+            </ul>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>10. Lexique</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            <ul>
+                <li>Utilisateur : Internaute se connectant, utilisant le site susnommé.</li>
+                <li>Informations personnelles : « les informations qui permettent, sous quelque forme que ce soit, directement ou non, l'identification des personnes physiques auxquelles elles s'appliquent » (article 4 de la loi n° 78-17 du 6 janvier 1978).</li>
+            </ul>
+        </div>
+
+        <div class="section-title">
+            <h2>Politique de confidentialité</h2>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            <em>
+                Devant le développement des nouveaux outils de communication, il est nécessaire de porter une attention particulière à la protection de la vie privée. C'est pourquoi, afin d'être en conformité avec le règlement général sur la protection des données (<strong><a href="https://www.aetherium.fr/rgpd-loi-de-finances-double-peine-impact-wordpress-ecommerce/" target="_blank" rel="noopener">RGPD</a></strong>) nous nous engageons à respecter la confidentialité des renseignements personnels que nous collectons.
+
+                En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier 1978, la loi n° 2004-801 du 6 août 2004, l'article L. 226-13 du Code pénal, la Directive Européenne du 24 octobre 1995 et enfin par le règlement n° 2016/679 (dit règlement général sur la protection des données ou RGPD).
+
+                En tout état de cause DUMILLIER Guillaume ne collecte des informations personnelles relatives à l'utilisateur que pour le besoin de certains services proposés par le site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a>. L'utilisateur fournit ces informations en toute connaissance de cause, notamment lorsqu'il procède par lui-même à leur saisie.
+            </em>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>1. Collecte des renseignements personnels</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            Les renseignements suivants sont collectés : Aucune information collectée.
+            </br>
+            Les renseignements personnels collectés sont recueillis au travers de formulaires et grâce à l'interactivité établie entre vous et le présent site Web. Les informations collectées sont conservées pour la durée nécessaire à la réalisation des finalités précisées précédemment. Sont également utilisé, comme indiqué dans la section suivante, des fichiers témoins et/ou journaux pour réunir des informations vous concernant.
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>2. Formulaires et interactivité</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            Vos renseignements personnels sont collectés par le biais de formulaires, à savoir : Aucun formulaire.
+            </br>
+            Les renseignements ainsi collectés sont utilisés pour les finalités suivantes : Non concerné.
+            </br>
+            Vos renseignements sont également collectés par le biais de l'interactivité pouvant s'établir entre vous et le présent site Web et ce, de la façon suivante :
+            <ul>
+                <li>- Visite sur le site</li>
+            </ul>
+            Les renseignements ainsi collectés le sont pour la finalité suivante :
+            <ul>
+                <li>- Statistiques de visites</li>
+            </ul>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>2.1 Droit d'opposition et de retrait</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            Je m'engage à vous offrir un droit d'opposition et de retrait quant à vos renseignements personnels. Le droit d'opposition s'entend comme étant la possibilité offerte aux internautes de refuser que leurs renseignements personnels soient utilisées à certaines fins mentionnées lors de la collecte. Le droit de retrait s'entend comme étant la possibilité offerte aux internautes de demander à ce que leurs renseignements personnels ne figurent plus, par exemple, dans une liste de diffusion.
+
+            Pour pouvoir exercer ces droits, vous pouvez contacter DUMILLIER Guillaume via ses liens sociaux.
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>2.2 Droit d'accès</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            Je m'engage à reconnaître un droit d'accès et de rectification aux personnes concernées désireuses de consulter, modifier, voire radier les informations les concernant.
+
+            L'exercice de ce droit se fera auprès de DUMILLIER Guillaume.
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>2.3 Sécurité</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            Les renseignements personnels collectés sont conservés dans un environnement sécurisé. Les informations de statistiques de visite sont effacées automatiquement au bout d'un an. Les personnes travaillant pour DUMILLIER Guillaume sont tenues de respecter la confidentialité de vos informations. Pour assurer la sécurité de vos renseignements personnels, les mesures suivantes sont mises en place : Protocole SSL (Secure Sockets Layer).
+            </br>
+            Aucune information personnelle de l'utilisateur du site <a href="https://www.gdumillier.ml">www.gdumillier.ml</a> n'est publiée à l'insu de l'utilisateur, échangée, transférée, cédée ou vendue sur un support quelconque à des tiers. Par ailleurs, je m'engage à maintenir un haut degré de confidentialité afin d'assurer la confidentialité de vos transactions. Toutefois, comme aucun mécanisme n'offre une sécurité maximale, une part de risque est toujours présente lorsque l'on utilise Internet pour transmettre des renseignements personnels.
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>3. Législation</h3>
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content about py-5">
+            Je m'engage à respecter les dispositions législatives énoncées dans le règlement européen n° 2016/679 (dit règlement général sur la protection des données ou RGPD).
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include("includes/footer.php") ?>
