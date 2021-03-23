219c0f704612db24832e632493bb14120e246535
Guillaume-dm <56759820+Guillaume-dm@users.noreply.github.com>
2021-03-23 10:17:03 +0100

    Create index.php
    Ajout du fichier index.php permettant d'afficher le contenu de mon porte-folio.

---
+++ index.php
@@ -0,0 +1,195 @@
+<!-- ======= Head ======= -->
+<?php include("includes/head.php") ?>
+<!-- End Head -->
+
+
+
+<!-- ======= Mobile nav toggle button ======= -->
+<button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+<?php include("includes/nav.php") ?>
+
+<?php include("includes/hero.php") ?>
+
+
+
+
+
+<!-- ======= About Section ======= -->
+<section id="about" class="about py-5">
+    <div class="container">
+
+        </br>
+
+        <div class="section-title">
+            <h2>A propos de moi ❓</h2>
+            <p>
+            <ul>
+                <li>2019, Bac STI2D </br>
+                    🏫 Lycée Jacob Holtzer (📍 Firminy - 42700) : Admis mention bien</li>
+                <li>2016, Brevet des Collèges </br>🏫 Collège Massenet-Fourneyron (📍 Le Chambon-Feugerolles - 42500) : Admis mention bien</li>
+            </ul>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-4" data-aos="fade-right">
+                <img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+                <h3>Développeur Odoo &amp; Développeur procédural</h3>
+                <p class="font-italic">
+                    J'ai des bases solides en C#, Phyton&Odoo, XML et HTML/CSS. J'ai déjà pratiqué le Php, le framework Laravel, le WinDev22, le Java et le JavaScript. J'aime avant tout la programation en procédurale et je m'intéresse de plus en plus à Odoo14.
+                </p>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <ul>
+                            <li><i class="icofont-rounded-right"></i> <strong>📍 Localisation :</strong> Disponible sur Firminy & Saint-Etienne (Loire)</li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6">
+                        <ul>
+                            <li><i class="icofont-rounded-right"></i> <strong>🕯️ Âge :</strong> 19</li>
+                            <li><i class="icofont-rounded-right"></i> <strong>Niveau :</strong> Débutant confirmé</li>
+                        </ul>
+                    </div>
+                </div>
+                <p>
+                    Vous souhaitez me contacter ou en apprendre davantage sur moi. Voici mon
+                    <a href="pdf/mon_CV.pdf" target="_blank"> CV <i class="icofont-file-pdf"></i></a>. N' hésitez pas à me contacter ou à regarder mes différents codes via mon GitHub.
+                </p>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- End About Section -->
+
+</br>
+
+<!-- ======= section Section ======= -->
+<section id="resume" class="section py-5">
+    <div class="container">
+
+        <div class="section-title">
+            <h2>Veille technologique 💻</h2>
+            <p>J'ai réalisé ma veille technologique sur le CMS Wordpress avec l'extension Woo commerce. J'ai choisi ce thème car je voulais découvrir le monde du e-commerce auquel je ne m'étais jamais intéressé auparavant. J'ai ainsi réalisé un site
+                de e-commerce axé sur l'informatique.
+            </p>
+            </br>
+            <img class="img-avant" src="demo_vt.png" alt="" /> <a href="pdf/Vfinal WordPress - WooCommerce.pdf" target="_blank"> 📽️ Voir le pdf de résumé de ma veille technologique.</a>
+        </div>
+    </div>
+</section>
+<!-- End section Section -->
+
+
+<!-- ======= section Section ======= -->
+<section id="portfolio" class="section py-5">
+    <div class="container">
+
+        <div class="section-title">
+            <h2>Veille juridique 🔨</h2>
+            <p>Voici les idées principales à retenir en matière de droit informatique :</p>
+            </br>
+            <p>
+                <li>La Jurisprudence du 6 Mai 2003 :</li>
+                <a href="https://www.maxenceperrinavocatdijon.fr/publications/commercial/laction-en-resolution-dun-contrat-de-prestation-informatique" target="_blank">
+                    <img class="img-avant" src="Jurisprudence élémentaire.png" alt="Voir la source original de l'extrait." />
+                </a>
+        </div>
+    </div>
+</section>
+<!-- End section Section -->
+<!-- ======= section Section ======= -->
+
+
+<section id="contact" class="section py-5">
+    <div class="container">
+        <div class="section-title">
+            <h2>Expériences & stages 🗂️</h2>
+            <ul>
+                <li>
+                    <p>
+                        Tout d'abord, vous pouvez consulter mon <a href="pdf/DUMILLIER%20Guillaume%20BTS%20SIO%20tableau%20CCF%20E6.xlsx%20-%20tableau%20de%20synth%C3%A8se%20SLAM.pdf" target="_blank"> tableau de compétences <i class="icofont-file-pdf"></i></a>.
+                        </br>
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        Stagiaire / <a href="https://www.adquat.com/contact/" target="_blank"> ADQUAT Solutions :</a>
+                        </br>
+                        📍 Saint-Chamond (42400)
+                        </br>
+                        📅 Janvier 2021 & Février 2021 (6 semaines)
+                        </br>
+                        📄 Réalisation via le Framework Odoo : </br>
+                        </br>
+                        🔸 D'un module amélioré d'OpenAcademy <em>(<i class="icofont-download"></i><a href="Apprentisage_ODOO-main.zip"> Télécharger mon module <i class="icofont-zipped"></i>)</a></em>.</br>
+                        <img class="img-avant" src="Avant vue session via cours.png" alt="" /> </br>
+                        </br>
+                        <!-- Sert à la séparation des listes des taches et des stages. -->
+                        🔸 D'un module pour les audits <em>(<i class="icofont-download"></i><a href="Module-Audit-main.zip"> Télécharger mon module <i class="icofont-zipped"></i>)</a></em>. </br>
+                        <img class="img-avant" src="Après la suppression des emojis de préconisations.png" alt="" /> </br>
+                        </br>
+                        🔸 D'une documentation utilisateur pour le module des congés. </br>
+                        <img class="img-avant" src="MDE2.png" alt="" />
+                        <a href="pdf/Total_documentation_conges.pdf" target="_blank"> <i class="icofont-file-pdf"></i> Voir la totalité de la documentation</a>
+                        </br>
+                    </p>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <p>
+                        Stagiaire / IEESS :
+                        </br>
+                        📍 Saint-Etienne (42000)
+                        </br>
+                        📅 Juin 2020 (4 semaines)
+                        </br>
+                        📄 Réalisation d'un projet de FTP en WinDev22, pour trier automatiquement les documents de mise à jour des clients.
+                        <img class="img-avant" src="Tables.png" alt="" />
+                        </br>
+                        </br>
+                        <img class="img-avant" src="rendu_stage1.png" alt="" />
+                        </br>
+                    </p>
+                </li>
+            </ul>
+            <ul>
+                <div>
+                    <li>
+                        Stagiaire / <a href="http://lasourcenumerique.fr/" target="_blank"> La source numérique </a>:
+                        </br>
+                        📍 Firminy (42700)
+                        </br>
+                        📅 Mars 2018 (5 jours)
+                        </br>
+                        📄 Aide pour divers types de public à l’initiation, aux bases de l’informatique, à domicile ou en déplacement.
+                    </li>
+                </div>
+            </ul>
+            <ul>
+                <div>
+                    <li>
+                        Stagiaire / Meubles Peyron (DIGITAL) :
+                        </br>
+                        📍 Le Chambon-Feugerolles (42500)
+                        </br>
+                        📅 Octobre 2015 (3 jours)
+                        </br>
+                        📄 Aide à l'installation et aux réparations dans l’électroménager et le multimédia chez les particuliers de l’entreprise.
+                    </li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!-- End section Section -->
+
+
+
+
+
+<!-- ======= Footer ======= -->
+<?php include("includes/footer.php") ?>
+<!-- End  Footer -->
