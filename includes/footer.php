a11e79600fb7c4b6246de51be5501e8c4b9b95e6
Guillaume-dm <56759820+Guillaume-dm@users.noreply.github.com>
2021-09-25 19:01:20 +0200

    Create footer.php
    Ajout du fichier footer.php de mon porte-folio.

---
+++ includes/footer.php
@@ -0,0 +1,51 @@
+       <section id="cookies">
+            <div class="container">
+                <div id="cookieConsent">
+                    <div id="closeCookieConsent">x</div>
+                    Ce site utilise des cookies pour améliorer votre navigation. <a href="../politique-de-confidentialite.php" target="_blank">Plus d'informations</a>. <a class="cookieConsentOK">J'accepte</a>
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+        </main>
+        <!-- End #main -->
+
+
+
+        <!-- ======= Footer ======= -->
+        <footer id="footer">
+            <div class="container">
+                <div class="copyright">
+                    &copy; Copyright 2021
+                </div>
+                <div class="credits">
+                    <!-- All the links in the footer should remain intact. -->
+                    <!-- You can delete the links only if you purchased the pro version. -->
+                    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/ -->
+                    Designed by Guillaume DUMILLIER.
+                    <a href="../politique-de-confidentialite.php">Politique de confidentialité </a>
+                </div>
+            </div>
+        </footer>
+        <!-- End  Footer -->
+
+
+        <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+        <!-- Vendor JS Files -->
+        <script src="assets/vendor/jquery/jquery.min.js"></script>
+        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+        <script src="assets/vendor/aos/aos.js"></script>
+
+        <!-- Template Main JS File -->
+        <script src="assets/js/main.js"></script>
+
+        </body>
+
+        </html>
